2f1c271608339af921b100bcf5f6e022d9fe0ca6
Igor Rocha <igortice@gmail.com>
2025-02-01 22:37:02 -0300

    feat: refactor transactions index to improve data handling; calculate totals in controller and update view for better performance

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,17 +1,24 @@
 class TransactionsController < ApplicationController
-  before_action :get_stores, only: %i[index]
+  before_action :get_stores, :get_transactions,
+                only: %i[index]
 
   def index
-    @transactions        = Transaction.includes(:store).order(date: :desc)
-    @transactions        = @transactions.where(store_id: params[:store_id]) if params[:store_id].present?
+    @total_sum      = @transactions.sum(:value)
+    @total_balance  = @transactions.total_balance
+    @total_incoming = @transactions.incoming.sum(:value)
+    @total_outgoing = @transactions.outgoing.sum(:value)
+
     @pagy, @transactions = pagy(@transactions)
-    @total_sum           = @transactions.sum(:value)
-    @total_balance       = @transactions.total_balance
   end
 
   private
 
   def get_stores
     @stores = Store.order(:name)
+  end
+
+  def get_transactions
+    @transactions        = Transaction.includes(:store).order(date: :desc)
+    @transactions        = @transactions.where(store_id: params[:store_id]) if params[:store_id].present?
   end
 end
